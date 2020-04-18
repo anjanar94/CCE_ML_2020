30a4b3c4cf725f73c1de7bad475618414183d825
harshit <agrrwal@gmail.com>
2020-04-18 13:07:45 +0530

    clean up

--- app_main.py
+++ app_main.py
@@ -4,8 +4,8 @@ python3 app_main
 """
 import os
 from ml.framework.file_utils import FileUtils
+FileUtils.mkdir('raw')
 from ml.ux import index
 
 if __name__ == '__main__':
-    FileUtils.mkdir('raw')
-    index
+    ""
